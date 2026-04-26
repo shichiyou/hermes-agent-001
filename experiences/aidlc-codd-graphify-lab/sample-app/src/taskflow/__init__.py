813c02c5ca4b5d77a92c816401ec81912ad1131c
Tanaka Yasunobu <shichiyou@outlook.com>
2026-04-26 11:26:37 +0000

    refactor: make aidlc codd graphify lab a submodule

--- experiences/aidlc-codd-graphify-lab/sample-app/src/taskflow/__init__.py
+++
@@ -1,7 +0,0 @@
-"""TaskFlow Mini sample application."""
-
-from taskflow.models import Task, TaskStatus
-from taskflow.repository import InMemoryTaskRepository
-from taskflow.service import TaskService
-
-__all__ = ["InMemoryTaskRepository", "Task", "TaskService", "TaskStatus"]
